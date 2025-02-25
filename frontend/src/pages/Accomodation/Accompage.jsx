aa6d5dc7f1255ad82577dd20fd88dd3129b9e998
Vaibhaviitian <aryanvaibhav74@gmail.com>
2025-02-25 08:38:26 +0530

    Error Debugging

--- frontend/src/pages/Accomodation/Accompage.jsx
+++ frontend/src/pages/Accomodation/Accompage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "./card.jsx";
+import Card from "./Card.jsx";
 import Navbar from "../../components/navbar/Navbar.jsx";
 import FooterT from "../../components/Footer/Footer.jsx";
 import DecryptedText from "../../components/reactBits/DecodeX.jsx";

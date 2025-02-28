1ab27c2c7d445822159948d4a260fa5473fe9be9
anirban2005143a <23je0104@iitism.ac.in>
2025-02-28 18:55:35 +0530

    merge git pull

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from './pages/homepage/Home.jsx'
@@ -9,18 +9,6 @@ import Eventpage from './pages/events/Eventpage.jsx'
 import AboutUsPage from './pages/About/About.jsx'
 import Accomodationpage from './pages/Accomodation/Accomodationpage.jsx'
 import Sponsors from './pages/Sponsors/Sponsors.jsx'
-=======
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/homepage/Home.jsx";
-import Teampage from "./pages/team/teampage.jsx";
-import Merchpage from "./pages/Merchandise/Merchpage.jsx";
-import bgImg from "/bgimg.jpeg";
-import Eventpage from "./pages/events/Eventpage.jsx";
-import AboutUsPage from "./pages/About/About.jsx";
-import Accomodationpage from "./pages/Accomodation/Accomodationpage.jsx";
-import RegisterPage from "./pages/Register/RegisterPage.jsx";
->>>>>>> 154c4c14270e8a77f458778128b0edb7898fa360
 const App = () => {
   return (
     <>
@@ -30,7 +18,7 @@ const App = () => {
       ></div>
       <BrowserRouter>
         <Routes>
-<<<<<<< HEAD
+
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<AboutUsPage />} />
           <Route path='/team' element={<Team />} />
@@ -38,15 +26,6 @@ const App = () => {
           <Route path='/Merchandise' element={<Merchpage />} />
           <Route path='/Accomodation' element={<Accomodationpage />} />
           <Route path='/Sponsors' element={<Sponsors />} />
-=======
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutUsPage />} />
-          <Route path="/team" element={<Teampage />} />
-          <Route path="/events" element={<Eventpage />} />
-          <Route path="/Merchandise" element={<Merchpage />} />
-          <Route path="/Accomodation" element={<Accomodationpage />} />
-          <Route path="/Registration" element={<RegisterPage />} />
->>>>>>> 154c4c14270e8a77f458778128b0edb7898fa360
         </Routes>
       </BrowserRouter>
     </>

38df5fdb9c61d24731afa771f44b2a07ed613bff
Anirban <157496311+anirban2005143a@users.noreply.github.com>
2025-03-03 15:23:05 +0530

    Update Dialogbox.jsx

--- frontend/src/pages/events/Dialogbox.jsx
+++ frontend/src/pages/events/Dialogbox.jsx
@@ -113,7 +113,7 @@ const GameDialog = ({ isOpen, onClose, imageUrl, gameInfo }) => {
             className="bg-neutral-900 rounded-xl shadow-2xl overflow-hidden lg:w-5/12 md:w-7/12 sm:w-10/12 w-full mx-4 h-[80%] min-h-[300px] flex flex-col"
           >
             {/* Image Section */}
-            <div className="relative w-full">
+            <div className="relative w-full h-[65%]">
               <img
                 src={imageUrl}
                 alt="Game"
@@ -142,7 +142,7 @@ const GameDialog = ({ isOpen, onClose, imageUrl, gameInfo }) => {
             </div>
 
             {/* Game Info Section */}
-            <div className="p-6">
+            <div className="p-6  h-[35%]">
               <h2 className="text-2xl font-bold text-white mb-4">
                 {gameInfo.title}
               </h2>
